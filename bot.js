7fad69c81f1c6f9c8297551bf15a74231762a662
Grefendor <lasse.grefe@tuhh.de>
2022-02-03 22:27:24 +0100

    Added Bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+console.log("Beep, Beep");
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.login('');
+
+client.on('ready', readyDiscord);
+
+function readyDiscord(){
+    console.log('Heart');
+}
